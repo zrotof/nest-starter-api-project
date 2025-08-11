4952b027b3741b2728ab56eebd2dab8cd71e7219
Samuel Mandeng <zrotof@Air-de-Samuel.lan>
2025-08-11 23:49:29 +0200

    updates

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], 
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+        synchronize: false,  // On désactive en prod pour utiliser migrations
+        migrationsRun: false, 
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
 export class DatabaseModule {}
